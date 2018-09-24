1ae54148f8fc2e77d1ff96071bbff32a98a18360
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-09-24 15:55:31 -0400

    Added questions and problems

---
+++ img/Questions on this project.js
@@ -0,0 +1,29 @@
+/*
+Questions and Problems I found from working on this project
+
+1. There is no image of individual product bottle(for Free Shipping Section) provided in the Building assets.
+
+2. Not provide text colors or sizes.  For example, red color in Perfect Blood Sugar in the Header Section
+
+3. Not provide color gradient background for Header Text (Perfect Blood Sugar)
+
+4. No indications for italic text (only on mockups which is not very clear)
+
+5. There are typo in text file (example, chcolate)
+
+6. Not provide text size (sometimes designer can be more specific)
+
+7. No Text shadow value provided but you have it on the mockup in the Old man  sitting at the tree section
+
+8. Two of the PNG images of Transparent Arrows are unusable (one is incompleted and one is not transparent)
+
+9. Text in the guarantee section that is provide is a bit different from the text in the mockup photo. (There are duplicated text in the mockup photo this in part)
+
+10. Font family provided are different than the mockup (You already indicated it on the instructor)
+
+11.(Suggestion) Footer text(the white text below the add to cart button) could have use text-align: center to make it look better.
+
+Thank you for your time.
+Rachada Chairangsaris (Bay)
+
+*/
